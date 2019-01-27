e5c4cf077f0dfdab20609634f0041a99e6ffd5d9
Sam <sam.bonfante@staydomio.com>
2019-01-27 12:32:31 -0500

    jump on all platforms

--- GGJ19/New Unity Project/Assets/NewBehaviourScript.cs
+++ GGJ19/New Unity Project/Assets/NewBehaviourScript.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-  public GameObject player;
-  public GameObject platform;
-  public GameObject ground;
-  private BoxCollider2D playerBox;
-  private BoxCollider2D platformBox;
-  private BoxCollider2D groundBox;
+    public GameObject player;
+    public List<GameObject> platforms;
+    public GameObject ground;
+    private BoxCollider2D playerBox;
+    private List<BoxCollider2D> platformBoxes;
+    private BoxCollider2D groundBox;
 
-  //movement variables
-  public int playerSpeedY = 1200;
-  public float playerSpeedX = 1.0f;
-  private float playerMoveX;
+    //movement variables
+    public int playerSpeedY = 1200;
+    public float playerSpeedX = 1.0f;
+    private float playerMoveX;
 
 
     private Animator animator;
@@ -26,47 +26,68 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-  {
-    player      = GameObject.Find("Player");
-    platform    = GameObject.Find("platform");
-    ground      = GameObject.Find("ground");
-    playerBox   = player.GetComponent<BoxCollider2D>();
-    platformBox = platform.GetComponent<BoxCollider2D>();
-    groundBox   = ground.GetComponent<BoxCollider2D>();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    PlayerMovement();
-  }
-
-  void PlayerMovement() {
-    // controls
-    if (Input.GetKeyDown(KeyCode.UpArrow)) {
-      if ( (playerBox.IsTouching(platformBox)) || (playerBox.IsTouching(groundBox)) ) {
-        Jump();
-      }
+    {
+        player = GameObject.Find("Player");
+        ground = GameObject.Find("ground");
+        playerBox = player.GetComponent<BoxCollider2D>();
+        groundBox = ground.GetComponent<BoxCollider2D>();
+        platforms = new List<GameObject>();
+        platformBoxes = new List<BoxCollider2D>();
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll<GameObject>())
+        {
+            Debug.Log(go.name);
+            if (go.name == "platform")
+            {
+                platforms.Add(go);
+                platformBoxes.Add(go.GetComponent<BoxCollider2D>());
+            }
+        }
     }
-    // Move the player, clamping them to within the boundaries 
-    // of the level.
-    var playerMoveX = Input.GetAxis("Horizontal");
-    playerMoveX *= (playerSpeedX * Time.deltaTime);
 
-    var newPosition = transform.position;
-    newPosition.x += playerMoveX;
-   animator.SetInteger("animState", 1);
-   transform.position = newPosition;
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // controls
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if ((playerBox.IsTouching(groundBox)))
+            {
+                Jump();
+            }
+            foreach (var platformBox in platformBoxes)
+            {
+                if (playerBox.IsTouching(platformBox))
+                {
+                    Jump();
+                    break;
+                }
+            }
+        }
+        // Move the player, clamping them to within the boundaries 
+        // of the level.
+        var playerMoveX = Input.GetAxis("Horizontal");
+        playerMoveX *= (playerSpeedX * Time.deltaTime);
+
+        var newPosition = transform.position;
+        newPosition.x += playerMoveX;
+        animator.Play("pip_walk");
+        transform.position = newPosition;
 
 
-    // player direction
+        // player direction
 
-    // physics
-    player.GetComponent<Rigidbody2D>().velocity = new Vector2 (playerMoveX * playerSpeedX, player.GetComponent<Rigidbody2D>().velocity.y);
-  }
+        // physics
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(playerMoveX * playerSpeedX, player.GetComponent<Rigidbody2D>().velocity.y);
+    }
 
-  void Jump() {
-    GetComponent<Rigidbody2D>().AddForce(Vector2.up*playerSpeedY);
-    animator.SetInteger("animState", 2);
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerSpeedY);
+        animator.Play("pip_jump", 0, 1);
     }
 }
